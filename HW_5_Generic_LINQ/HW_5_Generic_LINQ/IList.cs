063746188d2e62203e735ba0c6934f7f27ab72ea
Oleksii Kryvchykov <olexey81@gmail.com>
2023-06-20 09:30:17 +0300

    Start

---
+++ HW_5_Generic_LINQ/HW_5_Generic_LINQ/IList.cs
@@ -0,0 +1,6 @@
+﻿namespace HW_5_Generic_LINQ
+{
+    public interface IList : ICollection
+    {
+    }
+}
---
+++ HW_5_Generic_LINQ/HW_5_Generic_LINQ/LinkedList.cs
@@ -0,0 +1,184 @@
+﻿namespace HW_4_Collections
+{
+    internal class LinkedList : OneLinkedList
+    {
+        private LinkedNode? _first;
+        private LinkedNode? _last;
+        public override object? First => _first?.Data;
+        public override object? Last => _last?.Data;
+
+
+        public override void Add(object data)
+        {
+            LinkedNode newNode = new LinkedNode(data);
+
+            if (_first == null)
+            {
+                _first = newNode;
+                _last = newNode;
+            }
+            else
+            {
+                newNode.Previous = _last;
+                _last.Next = newNode;
+                _last = newNode;
+            }
+
+            _size++;
+        }
+
+        public override void AddFirst(object data)
+        {
+            LinkedNode newNode = new LinkedNode(data);
+
+            if (_first == null)
+            {
+                _first = newNode;
+                _last = newNode;
+            }
+            else
+            {
+                newNode.Next = _first;
+                _first.Previous = newNode;
+                _first = newNode;
+            }
+
+            _size++;
+        }
+
+        public void Remove(object data)
+        {
+            LinkedNode? current = _first;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (current == _first)
+                    {
+                        _first = (LinkedNode?)current.Next;
+                        if (_first != null)
+                            _first.Previous = null;
+                    }
+                    else if (current == _last)
+                    {
+                        _last = current.Previous;
+                        if (_last != null)
+                            _last.Next = null;
+                    }
+                    else
+                    {
+                        current.Previous.Next = current.Next;
+                        ((LinkedNode)(current.Next)).Previous = current.Previous;
+                    }
+
+                    _size--;
+
+                    break;
+                }
+
+                current = (LinkedNode?)current.Next;
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            if (_first == null)
+                return;
+
+            if (_first == _last)
+            {
+                _first = null;
+                _last = null;
+            }
+            else
+            {
+                _first = (LinkedNode?)_first.Next;
+                _first.Previous = null;
+            }
+
+            _size--;
+        }
+
+        public void RemoveLast()
+        {
+            if (_last == null)
+                return;
+
+            if (_first == _last)
+            {
+                _first = null;
+                _last = null;
+            }
+            else
+            {
+                _last = _last.Previous;
+                _last.Next = null;
+            }
+
+            _size--;
+        }
+
+        public override bool Contains(object data)
+        {
+            LinkedNode? current = _first;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+
+                current = (LinkedNode?)current.Next;
+            }
+
+            return false;
+        }
+
+        public override object[] ToArray()
+        {
+            object[] array = new object[_size];
+            LinkedNode current = _first;
+            int index = 0;
+
+            while (current != null)
+            {
+                array[index] = current.Data;
+                current = (LinkedNode?)current.Next;
+                index++;
+            }
+
+            return array;
+        }
+
+        public override void Clear()
+        {
+            _first = null;
+            _last = null;
+            _size = 0;
+        }
+
+        private class LinkedNode : OneLinkedNode
+        {
+            //public new LinkedNode? Next { get; set; } = null;
+            public LinkedNode? Previous { get; set; } = null;
+
+            public LinkedNode(object data) : base(data)
+            {
+            }
+        }
+    }
+}
+//  Двозв'язковий список.
+//  Являє собою елементи, пов'язані один з одним через посилання на наступний і попередній елементи.
+//  Сам список зберігає перший та останній елементи.
+
+//  Методи
+//  + Add(object),
+//  + AddFirst(object),
+//  + Remove(object),
+//  + RemoveFirst(),
+//  + RemoveLast(),
+//  + bool Contains(object),
+//  + ToArray(),
+//  + Clear()
+//  + Властивості – Count, First, Last – тільки для читання.
